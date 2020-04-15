e334a30fab69dd681cc326401ba26a519b4ad0da
ashadi cahyadi <ashadi.cc@gmail.com>
2020-04-15 20:25:24 +0700

    init commit

---
+++ app/Console/Commands/AdminUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+use Hash; 
+
+class AdminUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'create an admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = User::firstOrNew([
+            'username' => 'L-12',
+        ]); 
+
+        $user->username = 'L-12';
+        $user->name = 'Ashadi Cahyadi';
+        $user->email = 'ashadi.cc@gmail.com'; 
+        $user->password = Hash::make('k0mp1l451'); 
+        $user->save(); 
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">   
+        <title>{{ config('app.name', 'GMR_RT04') }}</title>
+        <link href="{{ asset('dist/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">GMR RT-04</h3></div>
+                                    <div class="card-body">
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress">Blok Rumah (Misal K-1)</label>
+                                                <input class="form-control py-4 @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus id="inputEmailAddress" placeholder="Masukkan blok rumah" />
+                                                @error('username')
+                                                    <div class="alert alert-danger" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputPassword">Password</label>
+                                                <input class="form-control py-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="inputPassword" type="password" placeholder="Masukkan password" />
+                                                @error('password')
+                                                    <div class="alert alert-danger" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" id="rememberPasswordCheck" name="remember" type="checkbox" /><label class="custom-control-label" for="rememberPasswordCheck">Ingat password</label></div>
+                                            </div>
+                                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="#">Lupa password? hubungi pak Handoko</a>
+                                                <input type="submit" class="btn btn-primary" value="Login">
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        GRAHA MANDIRI RESIDENCE RT-04
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <!-- <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; 2020 wirosableng</div>
+                        </div>
+                    </div>
+                </footer>
+            </div> -->
+        </div>
+        <script src="{{ asset('js/jquery-3.4.1.min.js') }}" ></script>
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('dist/js/scripts.js') }}"></script>
+    </body>
+</html>
