5bd025276063ea38a6e3f289e44934185de91939
ashadi cahyadi <ashadi.cc@gmail.com>
2020-04-25 23:34:19 +0700

    add loading user

--- resources/js/components/DetailUser.vue
+++ resources/js/components/DetailUser.vue
@@ -1,67 +1,76 @@
 <template>
     <div>
-        <h3>Tagihan Per Warga</h3>
-        <div class="form-group">
-            <label for="">Blok</label>
-            <input type="text" class="form-control" :value="this.user.blok" disabled>
+        <div v-show="loadingUser">
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div> 
         </div>
-        <div class="form-group">
-            <label for="">Nama</label>
-            <input type="text" class="form-control" :value="this.user.name" disabled>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-md-6">
-                <h5>Tagihan Bulan Sekarang ({{ this.currentMonth }}) </h5>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Iuran</th>
-                            <th>Nominal</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in thisMonth" :key="item.id">
-                            <td>{{ item.billing_name }}</td>
-                            <td>{{ formatAmount(item.amount) }}</td>
-                            <td>
-                                <status-row 
-                                :idRecord="item.id"
-                                :baseUrl="baseUrl"
-                                :statusProp="item.status"
-                                />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div v-show="!loadingUser">
+            <h3>Tagihan Per Warga</h3>
+            <div class="form-group">
+                <label for="">Blok</label>
+                <input type="text" class="form-control" :value="this.user.blok" disabled>
             </div>
-            <div class="col-md-6">
-                <h5>Tunggakan tagihan</h5>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Iuran</th>
-                            <th>Bulan/Tahun</th>
-                            <th>Nominal</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in otherMonth" :key="item.id">
-                            <td>{{ item.billing_name }}</td>
-                            <td>{{ item.month_name }} / {{ item.year }}</td>
-                            <td>{{ formatAmount(item.amount) }}</td>
-                            <td>
-                                <status-row 
-                                :idRecord="item.id"
-                                :baseUrl="baseUrl"
-                                :statusProp="item.status"
-                                />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div class="form-group">
+                <label for="">Nama</label>
+                <input type="text" class="form-control" :value="this.user.name" disabled>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>Tagihan Bulan Sekarang ({{ this.currentMonth }}) </h5>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Iuran</th>
+                                <th>Nominal</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in thisMonth" :key="item.id">
+                                <td>{{ item.billing_name }}</td>
+                                <td>{{ formatAmount(item.amount) }}</td>
+                                <td>
+                                    <status-row 
+                                    :idRecord="item.id"
+                                    :baseUrl="baseUrl"
+                                    :statusProp="item.status"
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <h5>Tunggakan tagihan</h5>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Iuran</th>
+                                <th>Bulan/Tahun</th>
+                                <th>Nominal</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in otherMonth" :key="item.id">
+                                <td>{{ item.billing_name }}</td>
+                                <td>{{ item.month_name }} / {{ item.year }}</td>
+                                <td>{{ formatAmount(item.amount) }}</td>
+                                <td>
+                                    <status-row 
+                                    :idRecord="item.id"
+                                    :baseUrl="baseUrl"
+                                    :statusProp="item.status"
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -91,7 +100,8 @@ export default {
             },
             otherMonth: [],
             thisMonth: [],
-            currentMonth: ''
+            currentMonth: '',
+            loadingUser: false
         }
     },
     watch: {
@@ -120,13 +130,16 @@ export default {
             let url = `${this.baseUrl}/user`
             this.thisMonth = []
             this.otherMonth = []
+            this.loadingUser = true
             axios.get(url, params).then(result => {
                 me.user = result.data.user 
                 me.thisMonth = result.data.thisMonth
                 me.otherMonth = result.data.otherMonth
                 me.currentMonth = result.data.currentMonth
+                me.loadingUser = false
             }).catch(e => {
                 me.errorMessage()
+                me.loadingUser = false
             })
         }
     }
